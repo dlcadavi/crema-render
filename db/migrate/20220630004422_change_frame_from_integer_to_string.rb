65130b9b0242121ed6dd4f098d471993bf9f937f
dlcadavi <dlcadavi@unal.edu.co>
2022-11-21 20:16:57 -0500

    eliminar la columna frame de students

--- db/migrate/20220630004422_change_frame_from_integer_to_string.rb
+++ db/migrate/20220630004422_change_frame_from_integer_to_string.rb
@@ -1,9 +1,2 @@
 class ChangeFrameFromIntegerToString < ActiveRecord::Migration[7.0]
-  def up
-    change_column :students, :frame, :string
-  end
-
-  def down
-    change_column :students, :frame, :integer
-  end
 end
